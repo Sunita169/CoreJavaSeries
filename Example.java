3d03fc5b154b2a1209c56a8913b962e9d9bf17ce
Sunita169 <sunitakammar2@gmail.com>
2026-04-10 21:46:31 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,7 @@
+class Example{
+public static void main(String [] args){
+ System.out.println("My Father name is Narayan");
+ System.out.println("My Mother name is Shridevi");
+ System.out.println("My Brother name is Sunil");
+}
+}
